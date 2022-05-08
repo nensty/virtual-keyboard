eac713bcaa68504f2f352bc7b0dfbbd5a5ea67fd
Anastasia Sazonova <anastasia.ilyukovich@pandadoc.com>
2022-05-08 17:28:55 +0200

    fix: fix deleting white-space

--- src/modules/utils/keyboardFunctionalKeysHandler.js
+++ src/modules/utils/keyboardFunctionalKeysHandler.js
@@ -11,7 +11,10 @@ const handlePressFunctionalKeys = (event, key, input) => {
     tempInput.innerHTML += "\n";
   }
 
-  if (key === "Backspace") {
+  if (key === "Backspace" || key === "Bksp") {
+    if (tempInput.innerHTML.endsWith("&nbsp;")) {
+      tempInput.innerHTML = tempInput.innerHTML.replace("&nbsp;", "");
+    }
     tempInput.innerHTML = tempInput.innerHTML.slice(0, -1);
   }
 
